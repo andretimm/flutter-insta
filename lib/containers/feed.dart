ec979874ea1707a636750d44375c5113e7c9934f
andretimm <andretimm2012@gmail.com>
2019-08-31 17:14:57 -0300

    Criado feed e bottom bar

--- lib/containers/feed.dart
+++ lib/containers/feed.dart
@@ -8,6 +8,106 @@ class Feed extends StatefulWidget {
 class _FeedState extends State<Feed> {
   @override
   Widget build(BuildContext context) {
-    return Text("Feed");
+    return Container(
+      padding: EdgeInsets.only(top: 5.0),
+      child: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Container(
+                      width: 30.0,
+                      height: 30.0,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                            fit: BoxFit.fill,
+                            image: NetworkImage(
+                                "https://x1.xingassets.com/assets/frontend_minified/img/users/nobody_m.original.jpg")),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 5.0),
+                    child: Text(
+                      "Profile 1",
+                      style:
+                          TextStyle(fontSize: 12, fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                ],
+              ),
+              IconButton(
+                icon: Icon(Icons.more_vert),
+                onPressed: () {},
+              ),
+            ],
+          ),
+          Container(
+            height: 250.0,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.contain,
+                image: NetworkImage(
+                    "https://x1.xingassets.com/assets/frontend_minified/img/users/nobody_m.original.jpg"),
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.favorite_border),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.chat_bubble_outline),
+                  ),
+                ],
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.bookmark_border),
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(left: 8.0),
+                child: Text(
+                  "123 Curtidas",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 11.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(left: 8.0, top: 1.0),
+                child: Text(
+                  "há 2 horas",
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 10.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,46 +1,109 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_insta/containers/feed.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _cIndex = 0;
+
+  void _incrementTab(index) {
+    setState(() {
+      _cIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        leading: IconButton(
-          icon: Icon(Icons.camera_alt),
-          color: Colors.black,
-          onPressed: () => {},
-        ),
-        title: IconButton(
-          icon: Image.asset('assets/instagram.png'),
-          iconSize: 90.0,
-          tooltip: 'Closes application',
-          onPressed: () => {},
-        ),
-        centerTitle: true,
-        actions: <Widget>[
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.live_tv),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            icon: Icon(Icons.camera_alt),
+            color: Colors.black,
+            onPressed: () => {},
           ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.send),
+          title: IconButton(
+            icon: Image.asset('assets/instagram.png'),
+            iconSize: 90.0,
+            tooltip: 'Volte ao inicio',
+            onPressed: () => {},
           ),
-        ],
-        actionsIconTheme: IconThemeData(
-          color: Colors.black,
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Text("Stories"),
-            Feed(),
+          centerTitle: true,
+          actions: <Widget>[
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.live_tv),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.send),
+            ),
           ],
+          actionsIconTheme: IconThemeData(
+            color: Colors.black,
+          ),
         ),
-      ),
-    );
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Stories"),
+              Feed(),
+              Feed(),
+              Feed(),
+              Feed(),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          currentIndex: _cIndex,
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+              ),
+              title: Text('Home'),
+              activeIcon: Icon(Icons.home, color: Colors.black),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search,
+              ),
+              title: Text('Search'),
+              activeIcon: Icon(Icons.search, color: Colors.black),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.add_box,
+              ),
+              title: Text('Search'),
+              activeIcon: Icon(Icons.add_box, color: Colors.black),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.favorite_border,
+              ),
+              title: Text('Search'),
+              activeIcon: Icon(Icons.favorite, color: Colors.black),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person_outline,
+              ),
+              title: Text('Search'),
+              activeIcon: Icon(Icons.person, color: Colors.black),
+            ),
+          ],
+          onTap: (index) {
+            _incrementTab(index);
+          },
+        ));
   }
 }
